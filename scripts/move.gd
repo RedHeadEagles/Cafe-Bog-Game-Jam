afb74f36af16442207b3191e9d67eb38ee797051
Ember <frostyemby@gmail.com>
2025-08-05 23:31:56 -0500

    jumps and gravity work properly

--- scripts/move.gd
+++ scripts/move.gd
@@ -1,34 +1,31 @@
 extends CharacterBody2D
 
 # how fast the player is
-@export var speed = 1000
-var gravity = 9.8
+@export var move_speed = 1000
+@export var jump_speed = 25000
 
 func _ready() -> void:
 	pass
 
-func _physics_process(delta: float) -> void:
-	velocity = Vector2.ZERO
-	
+# takes input and inserts it into velocity
+func user_input() -> void:
 	# if player presses left movement option, velocity decreases by player speed
 	if Input.is_action_pressed("left"):
-		velocity.x -= 1
+		velocity.x -= move_speed
 		
 	# if player presses right movement option, velocity increases by player speed
 	if Input.is_action_pressed("right"):
-		velocity.x += 1
+		velocity.x += move_speed
 		
 	if Input.is_action_pressed("down"):
-		velocity.y += 1
+		velocity.y += move_speed
 	
-	# if player presses jump movement option, increase in height
-	if Input.is_action_pressed("jump"):
-		velocity.y -= 1
-	
-	# normalize the movement vector and add gravity in
-	velocity = velocity.normalized() * speed * delta 
-	
-	# use results in the position
-	position += velocity #+ get_gravity()
-	
-	
+	# if player presses jump movement option and is on ground, increase in height
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y -= jump_speed
+
+func _physics_process(delta: float) -> void:
+	velocity = Vector2.ZERO
+	user_input()
+	velocity += get_gravity()
+	move_and_slide()
