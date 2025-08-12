ac037ae8e95ee6b9860a2e9271aabf118d947d7f
William Morlan <redheadeagles@gmail.com>
2025-08-12 13:07:05 -0600

    Implemented a double jump and a wall jump system. TODO: Remove @export on hasDoubleJump and hasWallJump in final game.

--- scripts/move.gd
+++ scripts/move.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 # how fast the player is
 @export var move_speed = 400
 @export var jump_speed = 600
+@export var hasDoubleJump = false
+@export var hasWallJump = false
+@export var wallKickback = 3
+var doubleJumpReady = 2
 
 func _ready() -> void:
 	pass
@@ -21,8 +25,30 @@ func user_input() -> void:
 		velocity.y += move_speed
 	
 	# if player presses jump movement option and is on ground, increase in height
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y -= jump_speed
+	
+	
+	if hasWallJump:
+		if is_on_wall():
+			doubleJumpReady += 1
+			if Input.is_action_just_pressed("jump") and (doubleJumpReady > 0):
+				velocity.y = -jump_speed
+				doubleJumpReady -= 1
+			if velocity.x < 0:
+				velocity.x = move_speed * wallKickback
+			else:
+				velocity.x = -(move_speed) * wallKickback
+	
+	if hasDoubleJump:
+		if is_on_floor():
+			doubleJumpReady = 2
+		if Input.is_action_just_pressed("jump") and (doubleJumpReady > 0):
+			velocity.y = -jump_speed
+			doubleJumpReady -= 1
+	else:
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = -jump_speed
+	
+
 
 func _physics_process(delta: float) -> void:
 	velocity.x = 0
@@ -30,10 +56,7 @@ func _physics_process(delta: float) -> void:
 	user_input() # get velocity vector from user input
 	
 	# fall slower, better jumps?
-	if velocity.y > 0:
-		velocity += get_gravity() * delta
-	else:
-		velocity += get_gravity() * delta
+	velocity += get_gravity() * delta
 	
 	# takes velocity and puts it into positon with collision detection
 	move_and_slide()
