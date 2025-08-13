228d58acb7ad3ae7e63b5a7531d0d9c0b3cbfd1c
Ember <frostyemby@gmail.com>
2025-08-13 01:40:45 -0500

    added basic settings menu functionality

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -10,7 +10,7 @@ func _on_continue_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
