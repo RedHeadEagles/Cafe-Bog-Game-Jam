18bd2ea3512349b686e2287ed89f157b6d4c5426
Ember <frostyemby@gmail.com>
2025-08-12 22:06:15 -0500

    added theming to make buttons/background more colorful

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,12 +1,15 @@
 extends Control
 
-func _on_play_pressed() -> void:
+func _ready() -> void:
+	pass
+
+func _on_new_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
+
+func _on_continue_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
-
-func _on_credits_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_exit_pressed() -> void:
