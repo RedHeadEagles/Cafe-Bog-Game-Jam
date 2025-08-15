3696517e90bbd46fc88a3b449d6cc838898e4a92
Ember <frostyemby@gmail.com>
2025-08-14 20:33:30 -0500

    tutorial level completed

--- scripts/give_prompt.gd
+++ scripts/give_prompt.gd
@@ -1,1 +1,15 @@
 extends Area2D
+
+
+func _on_body_entered(body: Node2D) -> void:
+	$"Perspective Prompt".visible = true
+
+
+# remove popup and area from memory
+func _on_perspective_prompt_confirmed() -> void:
+	queue_free()
+
+
+# remove popup and area from memory
+func _on_perspective_prompt_canceled() -> void:
+	queue_free()
