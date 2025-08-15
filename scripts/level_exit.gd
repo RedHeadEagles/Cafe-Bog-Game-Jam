4da30b1c5625b66ee74ce02f942495cffc04bd25
Ember <frostyemby@gmail.com>
2025-08-14 21:48:24 -0500

    start of first level

--- scripts/level_exit.gd
+++ scripts/level_exit.gd
@@ -2,4 +2,8 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://scenes/level1.tscn")
+	call_deferred('_next_level')
+
+
+func _next_level():
+	get_tree().change_scene_to_file('res://scenes/level1.tscn')
